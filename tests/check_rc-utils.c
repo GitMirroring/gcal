7664eaa97d87ed0ec28f73cc879aac958c23f1cb
Thorsten Alteholz <thorsten@alteholz.dev>
2025-09-29 18:08:48 +0000

    TEST: check calculation of distances

--- tests/check_rc-utils.c
+++ tests/check_rc-utils.c
@@ -121,6 +121,51 @@ START_TEST(test_m_between)
 }
 END_TEST
 
+START_TEST(test_biorythm)
+{
+    //XXX TODO has to be done
+    bool flag=true;
+    ck_assert(flag);
+
+}
+END_TEST
+
+/*
+ typedef struct coor_type
+ {
+  int lon_deg;
+  int lon_min;
+  int lon_sec;
+  int lat_deg;
+  int lat_min;
+  int lat_sec;
+  int the_mode;
+  int meters_above_sea_level;
+  int time_zone_in_mins;
+ }
+ Coor_struct;
+*/
+START_TEST(test_compute_distance)
+{
+    //XXX use 0 1 2 for the_mode
+    Coor_struct p1, p2;
+    double distance, compare;
+
+    p1.lon_deg=12; p1.lon_min=34; p1.lon_sec=45;
+    p1.lat_deg=34; p1.lat_min=56; p1.lat_sec=00;
+    p1.the_mode=0;
+
+    p2.lon_deg=00; p2.lon_min=56; p2.lon_sec=34;
+    p2.lat_deg=45; p2.lat_min=34; p2.lat_sec=12;
+    p2.the_mode=0;
+
+    compare=1543.190844;
+    distance=compute_distance(&p1, &p2);
+    printf("XXX distance: %f <-> %f\n",distance,compare);
+    ck_assert_double_eq_tol(distance, compare, 1e-2);
+}
+END_TEST
+
 Suite *gcal_suite_rc_utils(void)
 {
     Suite *s;
@@ -141,6 +186,8 @@ Suite *gcal_suite_rc_utils(void)
     tcase_add_test(tc_core, test_d_between);
     tcase_add_test(tc_core, test_w_between);
     tcase_add_test(tc_core, test_m_between);
+    tcase_add_test(tc_core, test_biorythm);
+    tcase_add_test(tc_core, test_compute_distance);
 
     suite_add_tcase(s, tc_core);
 

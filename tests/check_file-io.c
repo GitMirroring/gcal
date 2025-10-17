0160d3a11b2f9263b075e811012d06c4de30be7b
Thorsten Alteholz <thorsten@alteholz.dev>
2025-10-17 07:47:54 +0000

    TEST: start testing file-io

--- tests/check_file-io.c
+++ tests/check_file-io.c
@@ -1,11 +1,52 @@
 #include "../config.h"
 #include <check.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-START_TEST(test_gcal_dummy)
+#include "../src/tailor.h"
+#include "../src/common.h"
+#include "../src/globals.h"
+#include "../src/utils.h"
+#include "../src/file-io.h"
+#include "../src/tty.h"
+
+START_TEST(test_read_line)
 {
-    ck_assert_int_eq(254, 254);
+    FILE *f;
+    #define TESTFILE "test_file-io-read_line.output"
+    #define LINE1 "hurzilein"
+    #define LINE2 "blubber opt blah"
+    char *inputtext;
+
+    // prepare testfile
+    f=fopen(TESTFILE, "w");
+    sprintf (s1, _(LINE1));
+    print_text (f, s1);
+    sprintf (s1, _(LINE2));
+    print_text (f, s1);
+    fclose(f);
+
+    //read file again
+    printf("XXX read line\n");
+    f=fopen(TESTFILE, "r");
+/*
+char *file_read_line (FILE *fp, char **line_buffer, int *in_pool, char *pool, char *ptr_pool, const char *filename,
+                        long *line_number, int *line_length, const Fmode_enum mode, Bool *is_include, Bool *is_dvar,
+                        Bool *is_tvar)
+/ *!
+   Reads a line of a delivered resource/response file into `&line_buffer'
+     using the delivered char vector `pool', which must be allocated by caller
+     with size BUF_LEN+1 (BUF_LEN should be "A POWER OF 2", e.g., 4096).
+     Returns the position in buffer of the character managed in next call
+     by char pointer `ptr_pool', which must be defined by caller; or NULL
+     if EOF is detected.
+* /
+
+   inputtext=file_read_line(f, 
+*/
+    fclose(f);
 }
+
 END_TEST
 
 Suite *gcal_suite_file_io(char *testname)
@@ -18,7 +59,7 @@ Suite *gcal_suite_file_io(char *testname)
     /* Core test case */
     tc_core = tcase_create(testname);
 
-    tcase_add_test(tc_core, test_gcal_dummy);
+    tcase_add_test(tc_core, test_read_line);
 
     suite_add_tcase(s, tc_core);
 

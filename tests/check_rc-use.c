967043dbc3c289b105cf0bb036ebdd2d6d3c9335
Thorsten Alteholz <thorsten@alteholz.dev>
2025-11-19 08:44:05 +0000

    TEST: start testing rc_use()

--- tests/check_rc-use.c
+++ tests/check_rc-use.c
@@ -2,10 +2,22 @@
 #include <check.h>
 #include <stdlib.h>
 
+#include "../src/tailor.h"
+#include <ctype.h>
+#include "../src/common.h"
 
-START_TEST(test_gcal_dummy)
+#include "../src/globals.h"
+#include "../src/rc-defs.h"
+#include "../src/rc-use.h"
+
+START_TEST(test_settings)
 {
-    ck_assert_int_eq(254, 254);
+    //XXX maybe check other variables that have been set in rc_use()
+    ck_assert_pstr_ne(rc_bio_emo_lit,  _("Emo"));
+
+    rc_use();
+
+    ck_assert_pstr_eq(rc_bio_emo_lit,  _("Emo"));
 }
 END_TEST
 
@@ -19,7 +31,7 @@ Suite *gcal_suite_rc_use(char *testname)
     /* Core test case */
     tc_core = tcase_create(testname);
 
-    tcase_add_test(tc_core, test_gcal_dummy);
+    tcase_add_test(tc_core, test_settings);
 
     suite_add_tcase(s, tc_core);
 

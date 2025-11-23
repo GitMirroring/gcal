3c1fe1fe81f07ceda3b4602d62049d48e59d0f11
Thorsten Alteholz <thorsten@alteholz.dev>
2025-11-23 09:41:29 +0000

    TEST: start testing hd-data

--- tests/check_hd-data.c
+++ tests/check_hd-data.c
@@ -2,10 +2,39 @@
 #include <check.h>
 #include <stdlib.h>
 
+#include "../src/tailor.h"
+#include <ctype.h>
+#include "../src/common.h"
 
-START_TEST(test_gcal_dummy)
+#include "../src/globals.h"
+#include "../src/hd-astro.h"
+#include "../src/hd-defs.h"
+#include "../src/rc-defs.h"
+#include "../src/hd-data.h"
+#include "../src/tty.h"
+#include "../src/utils.h"
+#include "../src/hd-use.h"
+#include "../src/hd-data1.h"
+
+/*
+these functions are defined in a different check_* file and can be used here as well
+*/
+int length_of_hd_table();
+int length_of_cc_holidays_table();
+
+START_TEST(test_hdy)
 {
-    ck_assert_int_eq(254, 254);
+    int tableLength, hd_elems=0;
+    Bool init_data=true;
+    int cc_holidays_length = length_of_cc_holidays_table();
+    ck_assert_int_ne(cc_holidays_length, 0); // cc_holidays_length should be around 354
+
+    tableLength=length_of_hd_table(); ck_assert_int_eq(tableLength, 0);
+
+    printf("XXX au_hdy: init_data: %i  hd_elemes: %i\n",init_data, hd_elems);
+    au_hdy (&init_data, true, 0, 2025, &hd_elems, 0, 0);
+    printf("XXX au_hdy: init_data: %i  hd_elemes: %i\n",init_data, hd_elems);
+
 }
 END_TEST
 
@@ -19,7 +48,7 @@ Suite *gcal_suite_hd_data(char *testname)
     /* Core test case */
     tc_core = tcase_create(testname);
 
-    tcase_add_test(tc_core, test_gcal_dummy);
+    tcase_add_test(tc_core, test_hdy);
 
     suite_add_tcase(s, tc_core);
 
